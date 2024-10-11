a9663f496c113b1d50f9c3675ce08f6fb18a7dd2
Alexander <alexander@bokelund-singh.se>
2024-10-11 18:28:01 +0200

    Fixed movieLoader

--- src/loaders/movieLoader.jsx
+++ src/loaders/movieLoader.jsx
@@ -3,10 +3,8 @@ import safeFetchJson from "./utils";
 
 async function movieLoader(params) {
     const base = import.meta.env.VITE_API_BASE_URL;
-
     const movies = await safeFetchJson(base + "discover/movie?" + params);
-
-    await new Promise(resolve => setTimeout(resolve, 0));
     return Movie.parse(movies.results);
 }
+
 export default movieLoader;

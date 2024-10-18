dc57bf901d6d17a341679493b1484fcd8daefcfb
David Pettersson <david.pettersson@combain.com>
2024-10-18 14:11:34 +0200

    catch all route added

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter} from 'react-router-dom';
 import App from './App';
 import MoviePage from './components/MoviePage';
 import initLoader from './loaders/initLoader';
+import NotFound from './components/NotFound';
 
 const router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
                 index: true,
                 Component: MoviePage,
                 loader: initLoader,
+            },
+            {
+                path: "*",
+                Component: NotFound,
             }
         ]
     },
